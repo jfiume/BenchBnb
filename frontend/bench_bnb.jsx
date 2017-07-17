2d8505fffdce50477286b9c4f9bb77d305d39a5d
Joe Fiume <joefiume88@gmail.com>
2017-07-17 13:53:44 -0700

    added auth backend and apiajax calls

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {signup} from './util/session_api_util';
+import {login} from './util/session_api_util';
+import {logout} from './util/session_api_util';
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+
+// testing...
+window.login = login;
+window.signup = signup;
+window.logout = logout;
+
+
+
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
---
+++ state_shape.md
@@ -0,0 +1,20 @@
+State:
+
+{
+  session: {
+    currentUser: null,
+    errors: ["Invalid credentials"]
+  }
+}
+
+or this:
+
+{
+  session: {
+    currentUser: {
+      id: 1,
+      username: 'breakfast'
+    },
+    errors: []
+  }
+}
